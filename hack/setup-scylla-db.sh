97597611959e562a4fde1cc90d2b24ae5afac7dc
agwermann <alexandrewermann@gmail.com>
2023-07-15 22:44:50 -0300

    chore: increase timeout

--- hack/setup-scylla-db.sh
+++ hack/setup-scylla-db.sh
@@ -4,20 +4,20 @@ helm repo update
 
 # Configure cert-manager
 kubectl apply -f https://raw.githubusercontent.com/scylladb/scylla-operator/v1.9/examples/common/cert-manager.yaml
-kubectl wait -n cert-manager --for=condition=ready pod -l app=cert-manager --timeout=60s
+kubectl wait -n cert-manager --for=condition=ready pod -l app=cert-manager --timeout=200s
 
 # Configure operator with helm values
 helm install scylla-operator scylla/scylla-operator --values hack/scylla-operator/helm/values.operator.yaml --create-namespace --namespace scylla-operator
-kubectl wait -n scylla-operator --for=condition=ready pod -l app.kubernetes.io/name=scylla-operator --timeout=60s
+kubectl wait -n scylla-operator --for=condition=ready pod -l app.kubernetes.io/name=scylla-operator --timeout=200s
 
 # Configure manager with helm values
 helm install scylla-manager scylla/scylla-manager --values hack/scylla-operator/helm/values.manager.yaml --create-namespace --namespace scylla-manager
-kubectl wait -n scylla-manager --for=condition=ready pod -l app.kubernetes.io/name=scylla-manager --timeout=60s
-kubectl wait -n scylla-manager --for=condition=ready pod -l app.kubernetes.io/name=scylla-manager-controller --timeout=60s
+kubectl wait -n scylla-manager --for=condition=ready pod -l app.kubernetes.io/name=scylla-manager --timeout=200s
+kubectl wait -n scylla-manager --for=condition=ready pod -l app.kubernetes.io/name=scylla-manager-controller --timeout=200s
 
 # Configure scylla with helm values
 helm install scylla scylla/scylla --values hack/scylla-operator/helm/values.cluster.yaml --create-namespace --namespace scylla
-kubectl wait -n scylla --for=condition=ready pod -l app.kubernetes.io/name=scylla --timeout=60s
+kubectl wait -n scylla --for=condition=ready pod -l app.kubernetes.io/name=scylla --timeout=200s
 
 # Uninstall
 # helm uninstall scylla -n scylla
