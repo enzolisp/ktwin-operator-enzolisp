a9014e4baf8c30169a4bf6c3f0931a93ffbe94df
agwermann <alexandrewermann@gmail.com>
2023-07-22 19:02:38 -0300

    chore: upgrade knative to v 1.10

--- hack/setup-local.sh
+++ hack/setup-local.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH=$(dirname "$0")
 
-KNATIVE_VERSION=v1.8.0
+KNATIVE_VERSION=v1.10.0
 
 # RabbitMQ
 RABBITMQ_VERSION=v2.2.0
@@ -41,34 +41,34 @@ kubectl get pods --namespace knative-eventing
 kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
 
 # MQTT Deployment
-kubectl create namespace default
-kubectl apply -f ${SCRIPT_PATH}/mosquitto --namespace default
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace default
+# kubectl create namespace default
+# kubectl apply -f ${SCRIPT_PATH}/mosquitto --namespace default
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace default
 
 # RabbitMQ Cluster
-kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/download/${RABBITMQ_VERSION}/cluster-operator.yml
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
+# kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/download/${RABBITMQ_VERSION}/cluster-operator.yml
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
 
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/${RABBITMQ_CERT_MANAGER_VERSION}/cert-manager.yaml
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace cert-manager
+# kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/${RABBITMQ_CERT_MANAGER_VERSION}/cert-manager.yaml
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace cert-manager
 
-kubectl apply -f https://github.com/rabbitmq/messaging-topology-operator/releases/download/${RABBITMQ_MESSAGING_TOPOLOGY_OPERATOR_VERSION}/messaging-topology-operator-with-certmanager.yaml
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
+# kubectl apply -f https://github.com/rabbitmq/messaging-topology-operator/releases/download/${RABBITMQ_MESSAGING_TOPOLOGY_OPERATOR_VERSION}/messaging-topology-operator-with-certmanager.yaml
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
 
-# RabbitMQ Eventing
-kubectl apply -f https://github.com/knative-sandbox/eventing-rabbitmq/releases/download/knative-${KNATIVE_RABBITMQ_BROKER_VERSION}/rabbitmq-broker.yaml
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
+# # RabbitMQ Eventing
+# kubectl apply -f https://github.com/knative-sandbox/eventing-rabbitmq/releases/download/knative-${KNATIVE_RABBITMQ_BROKER_VERSION}/rabbitmq-broker.yaml
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
 
-# RabbitMQ Cluster
-kubectl apply -f ${SCRIPT_PATH}/rabbitmq-cluster -n default
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace default
-kubectl wait --for=condition=Ready --timeout=200s --all pods --namespace default
+# # RabbitMQ Cluster
+# kubectl apply -f ${SCRIPT_PATH}/rabbitmq-cluster -n default
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace default
+# kubectl wait --for=condition=Ready --timeout=200s --all pods --namespace default
 
-# RabbitMQ Broker
-kubectl apply -f ${SCRIPT_PATH}/rabbitmq-broker -n default
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace default
-kubectl wait --for=condition=Ready --timeout=200s --all pods --namespace default
+# # RabbitMQ Broker
+# kubectl apply -f ${SCRIPT_PATH}/rabbitmq-broker -n default
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
+# kubectl wait --for=condition=available --timeout=200s --all deployments --namespace default
+# kubectl wait --for=condition=Ready --timeout=200s --all pods --namespace default
 
 echo "Local setup script has finished"
