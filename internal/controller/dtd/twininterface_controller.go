14d8654a613d04d4b967e0209fb92ed9ff25ed1c
agwermann <alexandrewermann@gmail.com>
2023-08-19 18:24:08 -0300

    chore: remove comments

--- internal/controller/dtd/twininterface_controller.go
+++ internal/controller/dtd/twininterface_controller.go
@@ -50,15 +50,6 @@ type TwinInterfaceReconciler struct {
 //+kubebuilder:rbac:groups=dtd.ktwin,resources=twininterfaces/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=dtd.ktwin,resources=twininterfaces/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the TwinInterface object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
 func (r *TwinInterfaceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
