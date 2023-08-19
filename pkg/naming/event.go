13e0443635c7dcff04b63c19a915d73405a274f6
agwermann <alexandrewermann@gmail.com>
2023-08-19 19:01:45 -0300

    refactor: reuse constants

--- pkg/naming/event.go
+++ pkg/naming/event.go
@@ -1,25 +1,22 @@
 package naming
 
-import "fmt"
-
-const (
-	EVENT_REAL_TO_VIRTUAL string = "ktwin.real.%s"
-	EVENT_VIRTUAL_TO_REAL string = "ktwin.virtual.%s"
-	EVENT_TO_EVENT_STORE  string = "ktwin.event.store"
+import (
+	"fmt"
+	"ktwin/operator/pkg/event"
 )
 
 func GetEventTypeVirtualGenerated(twinInterfaceName string) string {
-	return fmt.Sprintf(EVENT_VIRTUAL_TO_REAL, twinInterfaceName)
+	return fmt.Sprintf(event.EVENT_VIRTUAL_TO_REAL, twinInterfaceName)
 }
 
 func GetEventTypeRealGenerated(twinInterfaceName string) string {
-	return fmt.Sprintf(EVENT_REAL_TO_VIRTUAL, twinInterfaceName)
+	return fmt.Sprintf(event.EVENT_REAL_TO_VIRTUAL, twinInterfaceName)
 }
 
 func GetNewCloudEventEventBinding(twinInterfaceName string) string {
-	return fmt.Sprintf(EVENT_REAL_TO_VIRTUAL, twinInterfaceName)
+	return fmt.Sprintf(event.EVENT_REAL_TO_VIRTUAL, twinInterfaceName)
 }
 
 func GetNewMQQTEventBinding(twinInterfaceName string) string {
-	return fmt.Sprintf(EVENT_REAL_TO_VIRTUAL, twinInterfaceName)
+	return fmt.Sprintf(event.EVENT_REAL_TO_VIRTUAL, twinInterfaceName)
 }
