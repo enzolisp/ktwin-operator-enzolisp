d555ca4b881a5f8673b69426ac8e82a31aaa4ff6
agwermann <alexandrewermann@gmail.com>
2023-12-09 13:39:54 -0300

    chore: change operators resources

--- hack/setup-brokers.sh
+++ hack/setup-brokers.sh
@@ -15,18 +15,26 @@ KNATIVE_RABBITMQ_BROKER_VERSION=v1.9.1
 kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/1-cluster-operator.yml
 kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
 
+#exit
+
 #kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/${RABBITMQ_CERT_MANAGER_VERSION}/cert-manager.yaml
 kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/2-cert-manager.yaml
 kubectl wait --for=condition=available --timeout=200s --all deployments --namespace cert-manager
+
+#exit
 
 #kubectl apply -f https://github.com/rabbitmq/messaging-topology-operator/releases/download/${RABBITMQ_MESSAGING_TOPOLOGY_OPERATOR_VERSION}/messaging-topology-operator-with-certmanager.yaml
 kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/3-messaging-topology-operator-with-certmanager.yaml
 kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
 
+#exit
+
 # RabbitMQ Eventing
 # kubectl apply -f https://github.com/knative-sandbox/eventing-rabbitmq/releases/download/knative-${KNATIVE_RABBITMQ_BROKER_VERSION}/rabbitmq-broker.yaml
 kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/4-rabbitmq-broker.yaml
 kubectl wait --for=condition=available --timeout=200s --all deployments --namespace knative-eventing
+
+#exit
 
 # RabbitMQ Cluster
 kubectl apply -f ${SCRIPT_PATH}/brokers/rabbitmq-cluster -n ktwin
