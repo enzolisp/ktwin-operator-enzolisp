7e5a7f85842e2cf748d36be327a330d4100fc9f9
agwermann <alexandrewermann@gmail.com>
2024-09-29 17:19:03 -0300

    chore: change scripts

--- hack/setup-brokers.sh
+++ hack/setup-brokers.sh
@@ -4,7 +4,6 @@ SCRIPT_PATH=$(dirname "$0")
 
 # RabbitMQ
 RABBITMQ_VERSION=v2.4.0
-RABBITMQ_CERT_MANAGER_VERSION=v1.11.1
 RABBITMQ_MESSAGING_TOPOLOGY_OPERATOR_VERSION=v1.10.3
 KNATIVE_RABBITMQ_BROKER_VERSION=v1.9.1
 
@@ -14,10 +13,6 @@ KNATIVE_RABBITMQ_BROKER_VERSION=v1.9.1
 kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/download/${RABBITMQ_VERSION}/cluster-operator.yml
 kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/1-cluster-operator.yml
 kubectl wait --for=condition=available --timeout=200s --all deployments --namespace rabbitmq-system
-
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/${RABBITMQ_CERT_MANAGER_VERSION}/cert-manager.yaml
-kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/2-cert-manager.yaml
-kubectl wait --for=condition=available --timeout=200s --all deployments --namespace cert-manager
 
 kubectl apply -f https://github.com/rabbitmq/messaging-topology-operator/releases/download/${RABBITMQ_MESSAGING_TOPOLOGY_OPERATOR_VERSION}/messaging-topology-operator-with-certmanager.yaml
 kubectl apply -f ${SCRIPT_PATH}/brokers/cluster-operator/3-messaging-topology-operator-with-certmanager.yaml
