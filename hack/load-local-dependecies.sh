60389d314f813be6290156197a0486b70a631914
enzolisp <enzolisp@gmail.com>
2025-06-04 21:26:14 -0300

    Cert-manager and scylla-operator webhook fault fix

--- hack/load-local-dependecies.sh
+++ hack/load-local-dependecies.sh
@@ -1,14 +1,24 @@
+# YOU NEED TO CREATE A GITHUB CLASSIC TOKEN TO BE ABLE TO PULL IMAGES FROM Open-Digital-Twin GROUP PACKAGES, THEN RUN:
+
+# echo YOUR_GITHUB_PAT | docker login ghcr.io -u YOUR_GITHUB_USERNAME --password-stdin
+
 # Pull KTWIN Containers
-docker pull dev.local/open-digital-twin/ktwin-event-store:0.1
-docker pull dev.local/open-digital-twin/ktwin-mqtt-dispatcher:0.1
-docker pull dev.local/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
-docker pull dev.local/open-digital-twin/ktwin-pole-service:0.1
+# docker pull dev.local/open-digital-twin/ktwin-event-store:0.1
+# docker pull dev.local/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+# docker pull dev.local/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
+# docker pull dev.local/open-digital-twin/ktwin-pole-service:0.1
+
+# Pull KTWIN Containers
+docker pull ghcr.io/open-digital-twin/ktwin-event-store:0.1
+docker pull ghcr.io/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+docker pull ghcr.io/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
+docker pull ghcr.io/open-digital-twin/ktwin-pole-service:0.1
 
 # Load KTWIN Containers into Kind
-kind load docker-image dev.local/open-digital-twin/ktwin-event-store:0.1
-kind load docker-image dev.local/open-digital-twin/ktwin-mqtt-dispatcher:0.1
-kind load docker-image dev.local/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
-kind load docker-image dev.local/open-digital-twin/ktwin-pole-service:0.1
+kind load docker-image ghcr.io/open-digital-twin/ktwin-event-store:0.1
+kind load docker-image ghcr.io/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+kind load docker-image ghcr.io/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
+kind load docker-image ghcr.io/open-digital-twin/ktwin-pole-service:0.1
 
 # Development utilities
 docker pull curlimages/curl:latest
--- hack/setup-scylla-db.sh
+++ hack/setup-scylla-db.sh
@@ -6,17 +6,32 @@ helm repo update
 kubectl apply -f https://raw.githubusercontent.com/scylladb/scylla-operator/v1.12.1/examples/common/cert-manager.yaml
 kubectl wait -n cert-manager --for=condition=ready pod -l app=cert-manager --timeout=200s
 
+# Patch cert-manager-webhook service to fix targetPort issue
+kubectl patch svc cert-manager-webhook -n cert-manager \
+  --type='json' \
+  -p='[{"op":"replace","path":"/spec/ports/0/targetPort","value":10250}]'
+
+
 # Configure operator with helm values
 helm install scylla-operator scylla/scylla-operator --values hack/scylla-operator/helm/values.operator.yaml --create-namespace --namespace scylla-operator
 kubectl wait -n scylla-operator --for=condition=ready pod -l app.kubernetes.io/name=scylla-operator --timeout=200s
 
+# Configure scylla-operator ClusterIssuer and Webhook Certificate
+kubectl apply -f hack/scylla-operator/ClusterIssuer.yml
+kubectl apply -f hack/scylla-operator/scylla-operator-webhook-cert.yml
+kubectl get secret scylla-operator-serving-cert -n scylla-operator -w
+
+kubectl wait -n scylla-operator --for=condition=ready pod -l app=webhook-server --timeout=120s
+
 # Configure manager with helm values
-helm install scylla-manager scylla/scylla-manager --values hack/scylla-operator/helm/values.manager.yaml --create-namespace --namespace scylla-manager
+#helm install scylla-manager scylla/scylla-manager --values hack/scylla-operator/helm/values.manager.yaml --create-namespace --namespace scylla-manager
+helm upgrade -i scylla-manager scylla/scylla-manager --values hack/scylla-operator/helm/values.manager.yaml --create-namespace --namespace scylla-manager
 kubectl wait -n scylla-manager --for=condition=ready pod -l app.kubernetes.io/name=scylla-manager --timeout=200s
 kubectl wait -n scylla-manager --for=condition=ready pod -l app.kubernetes.io/name=scylla-manager-controller --timeout=200s
 
 # Configure scylla with helm values
-helm install scylla scylla/scylla --values hack/scylla-operator/helm/values.cluster.yaml --create-namespace --namespace ktwin
+#helm install scylla scylla/scylla --values hack/scylla-operator/helm/values.cluster.yaml --create-namespace --namespace ktwin
+helm upgrade -i scylla scylla/scylla --values hack/scylla-operator/helm/values.cluster.yaml --create-namespace --namespace ktwin
 kubectl wait -n ktwin --for=condition=ready pod -l app.kubernetes.io/name=scylla --timeout=200s
 
 # Configure scylla cluster monitoring
