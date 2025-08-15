379bb34fe6f53f9856b5dfce38115592b2419541
enzolisp <enzolisp@gmail.com>
2025-08-15 11:04:14 -0300

    minikube initial config

---
+++ hack/setup-minikube.sh
@@ -0,0 +1,25 @@
+# YOU NEED TO CREATE A GITHUB CLASSIC TOKEN TO BE ABLE TO PULL IMAGES FROM Open-Digital-Twin GROUP PACKAGES, THEN RUN:
+
+# echo YOUR_GITHUB_PAT | docker login ghcr.io -u YOUR_GITHUB_USERNAME --password-stdin
+
+# Pull KTWIN Containers
+# docker pull dev.local/open-digital-twin/ktwin-event-store:0.1
+# docker pull dev.local/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+# docker pull dev.local/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
+# docker pull dev.local/open-digital-twin/ktwin-pole-service:0.1
+
+# Pull KTWIN Containers
+#docker pull ghcr.io/open-digital-twin/ktwin-event-store:0.1
+#docker pull ghcr.io/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+#docker pull ghcr.io/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
+#docker pull ghcr.io/open-digital-twin/ktwin-pole-service:0.1
+
+# Load KTWIN Containers into Minikube
+minikube image load ghcr.io/open-digital-twin/ktwin-event-store:0.1
+minikube image load ghcr.io/open-digital-twin/ktwin-mqtt-dispatcher:0.1
+minikube image load ghcr.io/open-digital-twin/ktwin-cloud-event-dispatcher:0.1
+minikube image load ghcr.io/open-digital-twin/ktwin-pole-service:0.1
+
+# Development utilities
+docker pull curlimages/curl:latest
+minikube image load curlimages/curl:latest
